d14f37a1da99e4d1455c7dd48002e42ca3fc12aa
lunker <dk910320@gmail.com>
2016-08-18 00:17:51 +0900

    move

--- LunkerLibrary/common/utils/NetworkManager.cs
+++ LunkerLibrary/common/utils/NetworkManager.cs
@@ -105,8 +105,6 @@ namespace LunkerLibrary.common.Utils
 
             Task readTask = Task.Factory.FromAsync(peer.BeginReceive(buff, 0, buff.Length, SocketFlags.None, null, peer), peer.EndReceive); // 생성과 동시에 실행됨
            
-            
-
             return readTask;
         }
         
